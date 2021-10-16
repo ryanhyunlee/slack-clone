f3e10663783d69bc5596dca85f4e7c44308860e8
ryanhyunlee <ryanjhlee87@outlook.com>
2021-10-16 16:43:35 +0900

    Add firebase/firestore

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,8 +1,23 @@
 import styled from "styled-components";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import SidebarOption from "./SidebarOption";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../firebase";
 
 const Sidebar = () => {
+  const [channels, loading, error] = useCollection(db.collection("rooms"));
+
   return (
     <SidebarContainer>
       <SidebarHeader>
@@ -15,7 +30,23 @@ const Sidebar = () => {
         </SidebarInfo>
         <CreateIcon />
       </SidebarHeader>
-      {/* 1:07 */}
+
+      <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+      <SidebarOption Icon={InboxIcon} title='Mentions & reactions' />
+      <SidebarOption Icon={DraftsIcon} title='Saved items' />
+      <SidebarOption Icon={BookmarkBorderIcon} title='Channel browser' />
+      <SidebarOption Icon={PeopleAltIcon} title='People & user groups' />
+      <SidebarOption Icon={AppsIcon} title='Apps' />
+      <SidebarOption Icon={FileCopyIcon} title='File browser' />
+      <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title='Show more' />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption title='Add Channel' />
+
+      {channels?.docs.map((doc) => (
+        <SidebarOption key={doc.id} id={doc.id} title={doc.data().name} />
+      ))}
     </SidebarContainer>
   );
 };
@@ -29,6 +60,12 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
---
+++ src/components/layout/SidebarOption.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../firebase";
+
+const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
+  const [channels, loading, error] = useCollection(db.collection("rooms"));
+
+  const addChannel = () => {
+    const channelName = prompt("Please enter the channel name");
+
+    if (channelName) {
+      db.collection("rooms").add({
+        name: channelName,
+      });
+    }
+  };
+
+  const selectChannel = () => {};
+
+  return (
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
+      {Icon && <Icon fontSize='small' style={{ padding: "10px" }} />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <SidebarOptionChannel>
+          <span>#</span> {title}
+        </SidebarOptionChannel>
+      )}
+    </SidebarOptionContainer>
+  );
+};
+
+export default SidebarOption;
+
+const SidebarOptionContainer = styled.div`
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.9;
+    background-color: #340e36;
+  }
+
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
+`;
+
+const SidebarOptionChannel = styled.h3`
+  padding: 10px 0;
+  font-weight: 300;
+`;
