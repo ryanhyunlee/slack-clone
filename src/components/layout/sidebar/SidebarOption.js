c600e9616d928d81834548f3dfc258406a3d4eae
ryanhyunlee <ryanjhlee87@outlook.com>
2021-10-25 18:11:06 +0900

    Add joyride

--- src/components/layout/sidebar/SidebarOption.js
+++ src/components/layout/sidebar/SidebarOption.js
@@ -1,13 +1,22 @@
 import { SidebarOptionContainer, SidebarOptionChannel } from "./SidebarStyles";
 
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { enterRoom } from "../../../actions";
+import ReactJoyride from "react-joyride";
 
 // Firebase /
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../../../firebase";
 
 const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
+  const [steps, setSteps] = useState([
+    {
+      target: ".showMsg",
+      content: "Add Channel or click channel to start chatting",
+    },
+  ]);
+
   const [channels, loading, error] = useCollection(db.collection("rooms"));
   const dispatch = useDispatch();
 
@@ -31,11 +40,12 @@ const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
     <SidebarOptionContainer
       onClick={addChannelOption ? addChannel : selectChannel}
     >
+      <ReactJoyride steps={steps} />
       {Icon && <Icon fontSize='small' style={{ padding: "10px" }} />}
       {Icon ? (
         <h3>{title}</h3>
       ) : (
-        <SidebarOptionChannel>
+        <SidebarOptionChannel className='showMsg'>
           <span>#</span> {title}
         </SidebarOptionChannel>
       )}
